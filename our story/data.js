130e22973576a68dfc02c39815bec9121c1cef6a
macapobre <macapobrejhonlloyd@gmail.com>
2026-06-19 23:01:42 +0800

    Update data.js

--- our story/data.js
+++ our story/data.js
@@ -10,19 +10,19 @@ const myTimelineData = [
     {
         date: "November 5, 2022",
         title: "The First Sight",
-        description: "The day we spoke for the first time and completely forgot about the rest of the room.",
+        description: "This was the day when I first saw you. I just came back from karate training in our university and you just joined the same team. And then I saw you and you captured my heart.",
         position: "left"
     },
     {
-        date: "September 21, 2023",
+        date: "February 21, 2023",
         title: "Making It Official",
-        description: "When we decided that 'you and me' sounded a whole lot better than just 'me'.",
+        description: "During this time we just finished our medical exam and decided to walk to Luneta Park and that's I decided to ask you out. I was so nervous but you said yes and it was the best day of my life.",
         position: "right"
     }
 ];
 
 // DEFAULT STARTING INSIDE JOKES
-const myInsideJokes = [
+const myInsideJokes= [
     "That time we spent 45 minutes looking for your phone while you were holding it.",
     "\"I'm not hungry\" — Proceeds to eat half of my dinner.",
     "The legendary battle over who gets the better side of the bed."
